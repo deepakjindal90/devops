c8cb26dabd7ae44c30f03ff00521d55d2a7c8287
Surender Singh Rathore <surenbika@gmail.com>
2017-09-19 20:21:03 +0530

    Init Command

---
+++ src/test/java/org/tektutor/AppTest.java
@@ -0,0 +1,14 @@
+package org.tektutor;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest 
+
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
